8f95cbbfba51e48e17137b5722ac9fb5845cf6b5
Isaac Gonzalez <gonzalezisaac0708@gmail.com>
2026-09-03 19:40:05 -0700

    Add terminal user input & added notes

--- src/main.py
+++ src/main.py
@@ -4,11 +4,19 @@ from openai import OpenAI
 
 load_dotenv()
 
+# Loading environment variables from .env file
 openai_api_key = os.getenv("OPENAI_API_KEY")
 
+# Indicating that Model Tank is initializing
 print("Model Tank initializing...")
 
+# Checking to see if the OPENAI_API_KEY environment variable is set 
 if openai_api_key is None:
     print("OPENAI_API_KEY was not found.")
 
+# Initializing the OpenAI client with the API key
 client = OpenAI(api_key = openai_api_key)
+
+# Checking to see if Model Tank is echoing user input
+user_message = input("User: ")
+print("Model Tank Received:", user_message)
